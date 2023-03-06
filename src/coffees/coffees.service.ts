265e2d8d4c69b99d487a5df1437523615b5d6f51
187PM14027 Nguyen Hoang Trung K24T <hoangtrung7762@gmail.com>
2023-03-07 01:09:05 +0700

    Lesson 28: Cascading inserts and updates

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -4,18 +4,24 @@ import { Repository } from 'typeorm';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 import { Coffee } from './entities/coffee.entity';
+import { Flavor } from './entities/flavor.entity';
 
 @Injectable()
 export class CoffeesService {
   constructor(
     @InjectRepository(Coffee)
     private readonly coffeeRepository: Repository<Coffee>,
+    @InjectRepository(Flavor)
+    private readonly flavorRepository: Repository<Flavor>,
   ) {}
   findAll() {
-    return this.coffeeRepository.find();
+    return this.coffeeRepository.find({ relations: ['flavors'] });
   }
   async findOne(id: string) {
-    const coffee = await this.coffeeRepository.findOneBy({ id: +id });
+    const coffee = await this.coffeeRepository.findOne({
+      where: { id: +id },
+      relations: ['flavors'],
+    });
     if (!coffee) {
       throw new NotFoundException(`The coffee #${id} does not exist.`);
     } else {
@@ -23,15 +29,28 @@ export class CoffeesService {
     }
   }
 
-  create(createCoffeeDto: CreateCoffeeDto) {
-    const coffee = this.coffeeRepository.create(createCoffeeDto);
+  async create(createCoffeeDto: CreateCoffeeDto) {
+    const flavors = await Promise.all(
+      createCoffeeDto.flavors.map((name) => this.preloadFlavorByName(name)),
+    );
+    const coffee = this.coffeeRepository.create({
+      ...createCoffeeDto,
+      flavors,
+    });
     return this.coffeeRepository.save(coffee);
   }
 
   async update(id: string, updateCoffeeDto: UpdateCoffeeDto) {
+    const flavors =
+      updateCoffeeDto.flavors &&
+      (await Promise.all(
+        updateCoffeeDto.flavors.map((name) => this.preloadFlavorByName(name)),
+      ));
+
     const updatedCoffee = await this.coffeeRepository.preload({
       id: +id,
       ...updateCoffeeDto,
+      flavors,
     });
     if (!updatedCoffee) {
       throw new NotFoundException(`Coffee #${id} not found`);
@@ -42,5 +61,14 @@ export class CoffeesService {
   async remove(id: string) {
     const coffeeIndex = await this.findOne(id);
     return this.coffeeRepository.remove(coffeeIndex);
+  }
+
+  private async preloadFlavorByName(name: string): Promise<Flavor> {
+    const existingFlavor = await this.flavorRepository.findOneBy({ name });
+    if (existingFlavor) {
+      return existingFlavor;
+    } else {
+      return this.flavorRepository.create({ name });
+    }
   }
 }
